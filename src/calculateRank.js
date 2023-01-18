a0d9267004838b3c45d40d2b8e85d372ee3821c8
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2023-01-18 11:25:28 +0200

    Update calculateRank.js

--- src/calculateRank.js
+++ src/calculateRank.js
@@ -63,11 +63,11 @@ const calculateRank = ({
     FOLLOWERS_OFFSET +
     REPO_OFFSET;
 
-  const RANK_S_VALUE = 1;
-  const RANK_DOUBLE_A_VALUE = 25;
-  const RANK_A2_VALUE = 45;
-  const RANK_A3_VALUE = 60;
-  const RANK_B_VALUE = 100;
+  const RANK_S_VALUE = 50;
+  const RANK_DOUBLE_A_VALUE = 60;
+  const RANK_A2_VALUE = 65;
+  const RANK_A3_VALUE = 90;
+  const RANK_B_VALUE = 150;
 
   const TOTAL_VALUES =
     RANK_S_VALUE +
